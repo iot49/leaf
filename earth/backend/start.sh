debed3e6ff76589aeeeb0864159d93f27375e0fc
Bernhard Boser <bernhard.e.boser@gmail.com>
2024-05-12 13:48:23 -0700

    fix /home/cloudflare/credentials

--- earth/backend/start.sh
+++ earth/backend/start.sh
@@ -2,10 +2,9 @@
 
 # credentials used by certbot
 cat > /home/cloudflare/credentials <<EOF
-# Cloudflare API credentials used by Certbot
-dns_cloudflare_email = ${CF_EMAIL}
-dns_cloudflare_api_key = ${CF_API_TOKEN}
+dns_cloudflare_api_token = "${CF_API_TOKEN}"
 EOF
+
 chmod 600 /home/cloudflare/credentials
 
 # fix ownership and permissions to edit files with code-server
@@ -18,3 +17,5 @@ setuidgid app uvicorn app.main:app --workers 3 --host 0.0.0.0 --port 8000
 # setuidgid app uvicorn alembic upgrade head && gunicorn -w 3 -k uvicorn.workers.UvicornWorker app.main:app  --bind 0.0.0.0:8000 --preload --log-level=debug --timeout 120
 
 # sleep infinity
+
+
