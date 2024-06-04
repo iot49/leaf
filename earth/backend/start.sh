b1df6d3d0ce8bcdbaa1fc368facb110dc9d37d9b
Bernhard Boser <bernhard.e.boser@gmail.com>
2024-06-03 21:19:23 -0700

    uvicorn: single worker

--- earth/backend/start.sh
+++ earth/backend/start.sh
@@ -15,10 +15,10 @@ chmod a+rx /home/letsencrypt/archive
 chown -R app:app /home/config
 chmod a+w /home/homeassistant/*.yaml  # homeassistant runs as root ...
 
-setuidgid app uvicorn app.main:app --workers 3 --host 0.0.0.0 --port 8000 --log-level error
+setuidgid app uvicorn app.main:app --workers 1 --host 0.0.0.0 --port 8000 --log-level error
 
-# setuidgid app uvicorn alembic upgrade head && uvicorn app.main:app --workers 3 --host 0.0.0.0 --port 8000
-# setuidgid app uvicorn alembic upgrade head && gunicorn -w 3 -k uvicorn.workers.UvicornWorker app.main:app  --bind 0.0.0.0:8000 --preload --log-level=debug --timeout 120
+# setuidgid app uvicorn alembic upgrade head && uvicorn app.main:app --workers 1 --host 0.0.0.0 --port 8000
+# setuidgid app uvicorn alembic upgrade head && gunicorn -w 1 -k uvicorn.workers.UvicornWorker app.main:app  --bind 0.0.0.0:8000 --preload --log-level=debug --timeout 120
 
 # sleep infinity
 
